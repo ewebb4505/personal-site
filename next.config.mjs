125ddc051ef3e34733fa818e977be9bf49d33f97
ewebb4505 <ewebb4505@gmail.com>
2024-06-02 17:20:34 -0400

    forgot this config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/personal-site",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
